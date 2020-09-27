30426cc01f311c64596372cd901f2fcd80e62010
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-09-27 11:09:55 +0200

    trying to get rid of sourcefiles in heroku + get heroku congif var to work.

--- src/store/mapAPI.js
+++ src/store/mapAPI.js
@@ -9,11 +9,13 @@ const api = axios.create({
   baseURL: 'https://api.mapbox.com/geocoding/v5/mapbox.places/',
 });
 const token = process.env.VUE_APP_MAPBOX_API_KEY;
+console.log(process.env.APIKEY);
+const token2 = process.env.APIKEY;
 
 const actions = {
   async geocodeLocation({ commit }, address) {
     let res = await api.get(
-      `${encodeURIComponent(address)}.json?access_token=${token}`
+      `${encodeURIComponent(address)}.json?access_token=${token || token2}`
     );
     if (res.data.features.length > 0) {
       await commit('geocoding_success', res.data.features[0].geometry);
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 //check if production mode is enabled, if so remove productionsourcemap.
-if (process.env.NODE_ENV == 'production') {
-  module.exports = { productionSourceMap: false };
-}
+module.exports = { productionSourceMap: false };
 
 module.exports = {
   transpileDependencies: ['vuetify'],
